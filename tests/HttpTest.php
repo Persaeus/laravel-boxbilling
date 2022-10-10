7d5a9e5e2f3cd7ebda516bb1dd7168fade5404c6
Snail Mail <32091869+nihilsen@users.noreply.github.com>
2022-10-10 07:38:28 +0000

    test: Add tests for client log in

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Client\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 use Nihilsen\FOSSBilling\Facades\FOSSBilling;
 
@@ -32,13 +33,30 @@ it('can query authenticated api endpoint', function () {
         );
 
         if ($isValidAuthenticatedRequest) {
-            return Http::response('{"result":["test passed"],"error":null}');
+            return Http::response('{"result":"test passed","error":null}');
         }
     });
 
     $messages = FOSSBilling::admin()->system_messages();
 
-    expect($messages)
-        ->toBeArray()
-        ->toBe(['test passed']);
+    expect($messages)->toBe('test passed');
+});
+
+it('attempts to log in via the admin API when querying the client API for a given client id', function () {
+    Http::fake(function (Request $request) {
+        $cookie = 'auth-cookie=auth-cookie-value';
+
+        if ($request->url() == 'https://fossbilling.invalid/api/admin/client/login') {
+            return Http::response(headers: ['Set-Cookie' => $cookie]);
+        }
+
+        if (
+            $request->url() == 'https://fossbilling.invalid/api/client/profile/get' &&
+            in_array($cookie, $request->header('Cookie'))
+        ) {
+            return Http::response('{"result":"logged in","error":null}');
+        }
+    });
+
+    expect(FOSSBilling::client(id: 1)->profile_get())->toBe('logged in');
 });
