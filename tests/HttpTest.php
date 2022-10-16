7da26b8133614c7ebaaa98e0856be692be15c4f7
Snail Mail <32091869+nihilsen@users.noreply.github.com>
2022-10-16 02:09:41 +0000

    test: Add tests for paginated API results

--- tests/HttpTest.php
+++ tests/HttpTest.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
+use Nihilsen\BoxBilling\Collection;
 use Nihilsen\BoxBilling\Facades\BoxBilling;
 
 it('can query guest api endpoint', function () {
@@ -23,8 +24,7 @@ it('can query guest api endpoint', function () {
 
 it('can query authenticated api endpoint', function () {
     Http::fake(function (Request $request) {
-        $isValidAuthenticatedRequest = (
-            $request->url() == 'https://boxbilling.invalid/api/admin/system/messages' &&
+        $isValidAuthenticatedRequest = ($request->url() == 'https://boxbilling.invalid/api/admin/system/messages' &&
             $request->hasHeader(
                 'Authorization',
                 'Basic '.base64_encode('admin:token')
@@ -58,4 +58,60 @@ it('attempts to log in via the admin API when querying the client API for a give
     });
 
     expect(BoxBilling::client(id: 1)->profile_get())->toBe('logged in');
+});
+
+it('handles paginated results', function () {
+    Http::fake(function (Request $request) {
+        if (! str_contains($request->url(), 'boxbilling.invalid/api/admin/client/get_list')) {
+            dd($request->url());
+
+            return;
+        }
+
+        parse_str(parse_url($request->url(), PHP_URL_QUERY), $result);
+
+        /** @var int|null $page */
+        extract($result);
+
+        if (empty($page) || $page == 1) {
+            return Http::response('{
+                "result": {
+                    "pages": 2,
+                    "page": 1,
+                    "per_page": 2,
+                    "total": "3",
+                    "list": [
+                        {
+                            "id": "1"
+                        },
+                        {
+                            "id": "2"
+                        }
+                    ]
+                },
+                "error": null
+            }');
+        }
+
+        if ($page == 2) {
+            return Http::response('{
+                "result": {
+                    "pages": 2,
+                    "page": 2,
+                    "per_page": 2,
+                    "total": "3",
+                    "list": [
+                        {
+                            "id": "3"
+                        }
+                    ]
+                },
+                "error": null
+            }');
+        }
+    });
+
+    expect(BoxBilling::admin()->client_get_list())
+        ->toBeInstanceOf(Collection::class)
+        ->toHaveCount(3);
 });
