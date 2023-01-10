48de4f3653c3ddbc5dc1bc8602b66cdf92ec4990
Snail Mail <32091869+nihilsen@users.noreply.github.com>
2023-01-10 02:58:09 +0000

    chore: Add phpdoc magic methods

--- src/API/Guest.php
+++ src/API/Guest.php
@@ -4,7 +4,8 @@ namespace Nihilsen\BoxBilling\API;
 
 /**
  * @method array{id: int, email: string, name: string, role: string} client_login(string $email, string $password)
- * @method string support_ticket_create(string $name, string $email, string $subject, string $message)
+ * @method array{id: int, title: string, slug: string, description: string, pricing: array} product_get(int $id, string $slug)
+ * @method string support_ticket_create(string $name, string $email, string $subject, string $message) Submit new public ticket
  * @method array{hash: string, author: array{name: string, email: string}, messages: array<array{content: string, author: array{name: string, email: string}, created_at: string, updated_at: string}>} support_ticket_get(string $hash)
  * @method string support_ticket_reply(string $hash, string $message)
  * @method string system_version() Get BoxBilling version
